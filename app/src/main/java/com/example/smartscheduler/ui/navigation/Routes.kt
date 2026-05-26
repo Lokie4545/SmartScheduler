85c5dd3cfcee1ecbcd701f8c3aa3c7a847a973e5
probe <probe@MacBook-Air-probe.local>
2026-05-26 18:36:38 +0300

    Added libs for Nav3

---
+++ app/src/main/java/com/example/smartscheduler/ui/navigation/Routes.kt
@@ -0,0 +1,13 @@
+package com.example.smartscheduler.ui.navigation
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Today: NavKey
+
+@Serializable
+object Calendar: NavKey
+
+@Serializable
+object Me: NavKey
